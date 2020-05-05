4eaf155f090e7262935c45da9ee43c497caac5a4
zuev720 <zuev720mail.ru>
2020-05-05 12:19:05 +0300

    style update

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -14,60 +14,49 @@
 <body>
 <div class="container header_information">
     <div class="row align-items-center">
-        <div class="col-lg-3"><h1 class="logotype">ИП Зуев</h1></div>
-        <div class="col-lg-6"><h4 class="header_description">Комплектующие собственного производства для чистых помещений</h4></div>
-        <div class="col-lg-3">
-            <div class="header_contacts no-gutters">
-                <div class="col"><img class="image_phone img-fluid align-text-bottom" src="https://img2.freepng.ru/20180402/age/kisspng-mobile-phones-computer-icons-telephone-clip-art-book-now-button-5ac21e3a728975.1107891215226711624692.jpg"/>+7 (909) 297-41-44</div>
-                <div class="col"><img class="image_mail img-fluid align-text-bottom" src="https://i2.wp.com/www.advokat35.ru/wp-content/uploads/2016/03/email-icon.png?fit=250%2C250"/> zuev720@mail.ru</div>
-                <div class="col"><a class="link_send_mail" href="mailto:zuev720@mail.ru"><img class="image_send_mail img-fluid align-text-bottom" src="https://w7.pngwing.com/pngs/570/333/png-transparent-email-address-computer-icons-symbol-email-miscellaneous-angle-orange-thumbnail.png"/>Связаться с нами</a></div>
+        <div class="col-lg"><h1 class="logotype">ИП Зуев</h1></div>
+        <div class="col-lg"><h4 class="header_description">Комплектующие собственного производства для чистых помещений</h4></div>
+        <div class="col-lg">
+            <div class="header_contacts align-items-center no-gutters">
+                <div class="col"><img class="image_phone img-fluid align-text-bottom" src="https://img2.freepng.ru/20180402/age/kisspng-mobile-phones-computer-icons-telephone-clip-art-book-now-button-5ac21e3a728975.1107891215226711624692.jpg">+7 (909) 297-41-44</div>
+                <div class="col"><img class="image_mail img-fluid align-text-bottom" src="https://i2.wp.com/www.advokat35.ru/wp-content/uploads/2016/03/email-icon.png?fit=250%2C250"> zuev720@mail.ru</div>
+                <div class="col"><a class="link_send_mail" href="mailto:zuev720@mail.ru"><img class="image_send_mail img-fluid align-text-bottom" src="https://w7.pngwing.com/pngs/570/333/png-transparent-email-address-computer-icons-symbol-email-miscellaneous-angle-orange-thumbnail.png" alt="">Связаться с нами</a></div>
             </div>
         </div>
     </div>
-</div>
-<nav class="menu container-fluid navbar navbar-expand-lg bg-light">
-    <button class="button_menu navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"><img class="img-fluid" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1200px-Hamburger_icon.svg.png"></span>
+    </div>
+<nav class="menu navbar navbar-expand-lg navbar-light" style="background-color: #2d3138">
+    <button class="button_menu navbar-toggler bg-light" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon align-self-center"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="menu_list container nav-pills navbar-nav nav-justified mr-auto" id="list">
-            <li class="nav-item"><a class="nav-link" href="{{ route('index') }}">ГЛАВНАЯ<span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item"><a class="nav-link" href="{{ route('products') }}">ПРОДУКЦИЯ</a>
-            </li>
-            <li class="nav-item"><a class="nav-link" href="{{ route('contacts') }}">КОНТАКТЫ</a>
-            </li>
-            <li class="nav-item"><a class="nav-link" id="about" href="{{ route('about') }}">О НАС</a>
-            </li>
+        <ul class="menu_list container nav-pills navbar-nav nav-justified mr-auto">
+            <a class="nav-item p-2" href="{{ route('index') }}"><li>ГЛАВНАЯ</li>
+            </a>
+            <a class="nav-item p-2" href="{{ route('products') }}"><li>ПРОДУКЦИЯ</li>
+            </a>
+            <a class="nav-item p-2" href="{{ route('contacts') }}"><li>КОНТАКТЫ</li>
+            </a>
+            <a class="nav-item p-2" href="{{ route('about') }}"><li>О НАС</li>
+            </a>
         </ul>
     </div>
 </nav>
 <div class="block_content container">
 @yield('content')
 </div>
-<footer class="mt-5">
-    <div class="www container mt-5">
-        <div class="footer_contacts_block row align-items-center">
-            <div class="col-lg-4"><h1 class="footer_logo">ИП Зуев</h1></div>
-            <div class="col-lg-5"><h5 class="footer_address">Нижегородская область г. Павлово<br> ул. Комунистическая 20</h5>
-            </div>
-            <div class="wrap_footer_contacts col-lg-3">
-                <div><img class="footer_contacts_image img-fluid" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSRRsjcDoB7chWkkgpuVq1y-XgosEwCp8Zk80dLIpzlqgfSs_mJ&usqp=CAU"><span class="footer_phone">+7(920)-057-10-12</span></div>
-                <div><img class="footer_contacts_image img-fluid" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQPSkxXAtI-K2jUbe_L_-eOCKM2cNZBGJ_I01FgdHPqp0EjxX9E&usqp=CAU"><span class="footer_email"> zuev720@mail.ru</span></div>
-            </div>
+<footer>
+<div class="footer_block container-fluid no-gutters">
+    <div class="container mt-5">
+        <div class="row text-center no-gutters">
+            <div class="col-md-4 mt-5"><h1 class="footer_logo row-fluid">ИП Зуев</h1></div>
+            <div class="col-md-4 mt-5"><h5 class="footer_address row-fluid">Нижегородская область г. Павлово<br> ул. Комунистическая 20</h5></div>
+            <div class="col-md-4 mt-5">Тел. +7 (909) 297-41-44</div>
         </div>
-        <div class="container">
-            <div class="footer-menu row mt-5">
-                <div class="col"><a class="flex-sm-fill text-sm-center nav-link" href="#">Производство</a></div>
-                <div class="col"><a class="flex-sm-fill text-sm-center nav-link" href="#">Продукция</a></div>
-                <div class="col"><a class="flex-sm-fill text-sm-center nav-link" href="#">О нас</a></div>
-            </div>
-        </div>
-        <div class="mt-5"><small>Работаем с 2015 года.</small></div>
+        <div class="row mt-5"><small>Работаем с 2015 года.</small></div>
     </div>
+</div>
 </footer>
-
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Товары')
 @section('content')
 <div class="card_product row m-0 p-0 justify-content-center">
-    <div class="card col-lg-4">
+    <div class="card col-lg-3 mr-2">
         <div class="container mt-3"><img class="image_card_product img-fluid card-img-top" src="https://proflineco.ru/upload/iblock/b9b/skrugl-el-1.png" alt="Card image cap"></div>
         <div class="card-body">
             <h4 class="card-title text-center">R 70</h4>
@@ -10,7 +10,7 @@
             <div class="row justify-content-center"><a href="#" class="card_button btn align-middle">Заказать</a></div>
         </div>
     </div>
-    <div class="card col-lg-4">
+    <div class="card col-lg-3 mr-2">
         <div class="container mt-3"><img class="image_card_product img-fluid card-img-top" src="https://proflineco.ru/upload/iblock/b9b/skrugl-el-1.png" alt="Card image cap"></div>
         <div class="card-body">
             <h4 class="card-title text-center">R 70</h4>
@@ -18,7 +18,7 @@
             <div class="row justify-content-center"><a href="#" class="card_button btn align-middle">Заказать</a></div>
         </div>
     </div>
-    <div class="card col-lg-4">
+    <div class="card col-lg-3 mr-2">
         <div class="container mt-3"><img class="image_card_product img-fluid card-img-top" src="https://proflineco.ru/upload/iblock/b9b/skrugl-el-1.png" alt="Card image cap"></div>
         <div class="card-body">
             <h4 class="card-title text-center">R 70</h4>
@@ -26,7 +26,7 @@
             <div class="row justify-content-center"><a href="#" class="card_button btn align-middle">Заказать</a></div>
         </div>
     </div>
-    <div class="card col-lg-4">
+    <div class="card col-lg-3 mr-2">
         <div class="container mt-3"><img class="image_card_product img-fluid card-img-top" src="https://proflineco.ru/upload/iblock/b9b/skrugl-el-1.png" alt="Card image cap"></div>
         <div class="card-body">
             <h4 class="card-title text-center">R 70</h4>
@@ -34,7 +34,7 @@
             <div class="row justify-content-center"><a href="#" class="card_button btn align-middle">Заказать</a></div>
         </div>
     </div>
-    <div class="card col-lg-4">
+    <div class="card col-lg-3 mr-2">
         <div class="container mt-3"><img class="image_card_product img-fluid card-img-top" src="https://proflineco.ru/upload/iblock/b9b/skrugl-el-1.png" alt="Card image cap"></div>
         <div class="card-body">
             <h4 class="card-title text-center">R 70</h4>
@@ -42,7 +42,7 @@
             <div class="row justify-content-center"><a href="#" class="card_button btn align-middle">Заказать</a></div>
         </div>
     </div>
-    <div class="card col-lg-4">
+    <div class="card col-lg-3 mr-2">
         <div class="container mt-3"><img class="image_card_product img-fluid card-img-top" src="https://proflineco.ru/upload/iblock/b9b/skrugl-el-1.png" alt="Card image cap"></div>
         <div class="card-body">
             <h4 class="card-title text-center">R 70</h4>
